9fd9bf67cf9b2cde654e505d9c0797d35f65525e
Braulio Chávez Martínez <balochavezmtz99@hotmail.com>
2023-10-08 20:50:50 -0600

    Fix for audio

--- src/app/entries/entries.component.ts
+++ src/app/entries/entries.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
-import { IEntry, IEntryResolved } from 'src/shared/interfaces';
+import { IEntryResolved } from 'src/shared/interfaces';
 
 @Component({
   selector: 'app-entries',
@@ -10,16 +10,37 @@ import { IEntry, IEntryResolved } from 'src/shared/interfaces';
 })
 export class EntriesComponent implements OnInit {
   title = 'invites';
+
+  audio = new Audio()
   
   entryResolved!: IEntryResolved;
   entryResolvedEmiter$ = new Subject<IEntryResolved>();
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) { 
+    this.audio.src = '../../assets/soundtrack.mp3'
+    this.audio.load()
+  }
   
   ngOnInit(): void {
     this.route.data.subscribe(data => {
       this.entryResolved = this.route.snapshot.data['entry'];
       this.entryResolvedEmiter$.next(this.entryResolved)
     })
+
+    const audioButton = (document.querySelector('.audioButton') as HTMLButtonElement).click()
+  }
+
+  playAudio() {
+    const playButton = (document.querySelector('.audioButton i.fa-play') as HTMLElement)
+    const pauseButton = (document.querySelector('.audioButton i.fa-pause') as HTMLElement)
+    if (this.audio.paused) {
+      this.audio.play()
+      playButton.style.display = 'none'
+      pauseButton.style.display = 'inline-block'
+    } else {
+      this.audio.pause()
+      playButton.style.display = 'inline-block'
+      pauseButton.style.display = 'none'
+    }
   }
 }
