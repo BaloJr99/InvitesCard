15c150ab9346e4f61751dfb4352906df8f570061
Braulio Chávez Martínez <balochavezmtz99@hotmail.com>
2023-10-16 21:42:10 -0600

    Checking other possible fix pt3

--- src/app/entries/countdown/countdown.component.ts
+++ src/app/entries/countdown/countdown.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Subscription, interval } from 'rxjs';
+import { BehaviorSubject, Subscription, interval } from 'rxjs';
+import { ITimer } from 'src/shared/interfaces';
 
 @Component({
   selector: 'app-countdown',
@@ -7,22 +8,13 @@ import { Subscription, interval } from 'rxjs';
   styleUrls: ['./countdown.component.css']
 })
 export class CountdownComponent implements OnInit {
-  time!: {
-    days: number;
-    hours: number;
-    minutes: number;
-    seconds: number;
-  };
+  time$ = new BehaviorSubject<ITimer>({ days: 0, hours: 0, minutes: 0, seconds: 0 })
 
   finishDate: Date = new Date();
 
   subscription!: Subscription;
 
   ngOnInit(): void {
-    this.time = {
-      days: 0, hours: 0, minutes: 0, seconds: 0
-    };
-
     this.finishDate = new Date('2023-11-18:19:00:00')
     
     this.subscription = interval(1000).subscribe(() => {
@@ -34,19 +26,20 @@ export class CountdownComponent implements OnInit {
     
     const now = new Date();
     const diff = this.finishDate.getTime() - now.getTime();
-    console.log(diff)
 
     // Cálculos para sacar lo que resta hasta ese tiempo objetivo / final
     const days = Math.floor(diff / (1000 * 60 * 60 * 24));
     const hours = Math.floor(diff / (1000 * 60 * 60));
     const mins = Math.floor(diff / (1000 * 60));
     const secs = Math.floor(diff / 1000);
-    
+
     // La diferencia que se asignará para mostrarlo en la pantalla
-    this.time.days = days;
-    this.time.hours = hours - days * 24;
-    this.time.minutes = mins - hours * 60;
-    this.time.seconds = secs - mins * 60;
+    const time = {
+      days, 
+      hours: hours - days * 24, 
+      minutes: mins - hours * 60, 
+      seconds: secs - mins * 60
+    };
+    this.time$.next(time);
   }
-  
 }
