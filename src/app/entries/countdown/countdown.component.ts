76d59a80145514e90ede7f685a5ea4b72f103123
Braulio Chávez Martínez <balochavezmtz99@hotmail.com>
2023-10-16 21:29:43 -0600

    Checking other possible fix pt2

--- src/app/entries/countdown/countdown.component.ts
+++ src/app/entries/countdown/countdown.component.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-countdown',
@@ -6,38 +7,46 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
   styleUrls: ['./countdown.component.css']
 })
 export class CountdownComponent implements OnInit {
-  days!: number;
-  hours!: number;
-  minutes!: number;
-  seconds!: number;
+  time!: {
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+  };
+
+  finishDate: Date = new Date();
+
+  subscription!: Subscription;
 
   ngOnInit(): void {
-    this.days = 0;
-    this.hours = 0;
-    this.minutes = 0;
-    this.seconds = 0;
+    this.time = {
+      days: 0, hours: 0, minutes: 0, seconds: 0
+    };
+
+    this.finishDate = new Date('2023-11-18:19:00:00')
     
-    setInterval(() => {
-      this.updateDate();
-    }, 1000);
+    this.subscription = interval(1000).subscribe(() => {
+      this.updateTime();
+    })
   }
 
-  diffTime = Math.abs(new Date().valueOf() - new Date('2023-11-18:19:00:00').valueOf());
+  updateTime() {
+    
+    const now = new Date();
+    const diff = this.finishDate.getTime() - now.getTime();
+    console.log(diff)
 
-  updateDate(): void {
-    this.days = this.diffTime / (24 * 60 * 60 * 1000);
-    this.hours = (this.days % 1) * 24;
-    this.minutes = (this.hours % 1) * 60;
-    this.seconds = (this.minutes % 1) * 60;
-    [ this.days, this.hours, this.minutes, this.seconds ] = 
-      [
-        Math.floor(this.days), 
-        Math.floor(this.hours), 
-        Math.floor(this.minutes), 
-        Math.floor(this.seconds)
-      ];
-
-    this.diffTime = Math.abs(new Date().valueOf() - new Date('2023-11-18:19:00:00').valueOf());
+    // Cálculos para sacar lo que resta hasta ese tiempo objetivo / final
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(diff / (1000 * 60 * 60));
+    const mins = Math.floor(diff / (1000 * 60));
+    const secs = Math.floor(diff / 1000);
+    
+    // La diferencia que se asignará para mostrarlo en la pantalla
+    this.time.days = days;
+    this.time.hours = hours - days * 24;
+    this.time.minutes = mins - hours * 60;
+    this.time.seconds = secs - mins * 60;
   }
   
 }
