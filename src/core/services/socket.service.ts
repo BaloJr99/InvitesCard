8718de0f96ba55dec7b56cd22c9e788bca7c1b8d
Braulio Chávez Martínez <balochavezmtz99@hotmail.com>
2023-11-09 22:27:37 -0600

    Added env variable to notifications

--- src/core/services/socket.service.ts
+++ src/core/services/socket.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from "@angular/core";
 import { io } from "socket.io-client";
+import { environment } from "src/environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketService {
-  io = io("http://localhost:3000", {
+  io = io(environment.apiUrl, {
     withCredentials: true,
     autoConnect: true
   })
