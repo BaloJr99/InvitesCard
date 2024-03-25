38c57923b07d11db732d8fc0a511e639e18721d0
Braulio Chávez Martínez <balochavezmtz99@hotmail.com>
2024-03-24 20:13:29 -0600

    Trying to add meta tags in resolver

--- src/app/entries/entries-resolver.service.ts
+++ src/app/entries/entries-resolver.service.ts
@@ -1,4 +1,5 @@
 import { inject } from "@angular/core";
+import { Meta } from "@angular/platform-browser";
 import { ActivatedRouteSnapshot, ResolveFn, Router } from "@angular/router";
 import { EMPTY, Observable, catchError, map } from "rxjs";
 import { EntriesService } from "src/core/services/entries.service";
@@ -9,6 +10,7 @@ export const entriesResolver: ResolveFn<IEntryResolved> = (
   
   const entriesService = inject(EntriesService);
   const router = inject(Router);
+  const meta = inject(Meta);
 
   const id = route.paramMap.get('id') ?? "";
 
@@ -19,6 +21,22 @@ export const entriesResolver: ResolveFn<IEntryResolved> = (
       return EMPTY
     })
   )
+
+  meta.addTags([
+    {
+      property: 'og:title',
+      content: 'Testing Meta'
+    },
+    {
+      name: 'og:description',
+      content: 'Testing description'
+    },
+    {
+      property: 'og:image',
+      itemprop: 'image',
+      content: 'https://img.freepik.com/vector-gratis/panda-lindo-bambu_138676-3053.jpg?size=338&ext=jpg&ga=GA1.1.1319243779.1711238400&semt=ais'
+    }
+  ])
 
   return entryFound;
 } 
