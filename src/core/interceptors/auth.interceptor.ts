95444ee03b2f1c3db0259b6f56c9822c563f3c65
Braulio Chávez Martínez <balochavezmtz99@hotmail.com>
2024-04-09 23:19:04 -0600

    Update to JWT

--- src/core/interceptors/auth.interceptor.ts
+++ src/core/interceptors/auth.interceptor.ts
@@ -3,7 +3,7 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { TokenStorageService } from "../services/token-storage.service";
 
-const TOKEN_HEADER_KEY = 'x-access-token';
+const TOKEN_HEADER_KEY = 'Authorization';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -14,7 +14,7 @@ export class AuthInterceptor implements HttpInterceptor {
 
     const token = this.token.getToken();
     if (token != null) {
-      authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, token)})
+      authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, `Bearer ${token}`)});
     }
 
     return next.handle(authReq);
