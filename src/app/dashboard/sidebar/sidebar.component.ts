918f9b95cac11b4d420c7b34db315e6af7d48607
Braulio Chávez Martínez <balochavezmtz99@hotmail.com>
2024-08-18 18:20:07 -0600

    Small tweak admin issue

--- src/app/dashboard/sidebar/sidebar.component.ts
+++ src/app/dashboard/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { TokenStorageService } from 'src/core/services/token-storage.service';
+import { Roles } from 'src/shared/enum';
 import { IEvent } from 'src/shared/interfaces';
 
 @Component({
@@ -6,11 +8,22 @@ import { IEvent } from 'src/shared/interfaces';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   @Input() events: IEvent[] = [];
   @Input() eventSelected!: IEvent;
 
+  isAdmin = false;
+
   @Output() getEventInformation: EventEmitter<IEvent> = new EventEmitter();
+
+  constructor(private tokenService: TokenStorageService) { }
+
+  ngOnInit(): void {
+    const userInformation = this.tokenService.getTokenValues();
+    if (userInformation) {
+      this.isAdmin = userInformation.roles.some(r => r.name == Roles.Admin);
+    }
+  }
 
   getEventEntries(id: string | undefined) {
     if (id) {
