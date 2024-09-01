faf405445051ba0685358bd32314ab6d451fde20
Braulio Chávez Martínez <balochavezmtz99@hotmail.com>
2024-08-31 18:51:35 -0600

    Renamed file

--- src/app/dashboard/events/events.module.ts
+++ src/app/dashboard/events/events.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { SharedModule } from "src/app/shared/shared.module";
 import { RouterModule, Routes } from "@angular/router";
 import { EventsComponent } from "./events.component";
-import { eventResolver } from "./event-details/event-resolver.service";
+import { eventResolver } from "./eventDetails/event-resolver.service";
 import { EventsService } from "src/app/core/services/events.service";
 import { UsersService } from "src/app/core/services/users.service";
 import { DateFormatPipe } from "src/app/shared/pipes/date-format.pipe";
@@ -15,7 +15,7 @@ const routes: Routes = [
   },
   {
     path: ':id',
-    loadChildren: () => import('./event-details/event-details.module').then(m => m.EventDetailsModule),
+    loadChildren: () => import('./eventDetails/event-details.module').then(m => m.EventDetailsModule),
     resolve: { eventResolved: eventResolver }
   }
 ]
