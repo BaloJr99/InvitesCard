6f762451010498e0ea8dfedbb191a648f754de09
Braulio Chávez Martínez <balochavezmtz99@hotmail.com>
2024-09-09 21:48:48 -0600

    Updated users component

--- src/app/dashboard/users/users.component.ts
+++ src/app/dashboard/users/users.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { DataTableDirective } from 'angular-datatables';
 import { ADTSettings } from 'angular-datatables/src/models/settings';
 import { Subject } from 'rxjs';
@@ -9,15 +9,44 @@ import { UsersService } from 'src/app/core/services/users.service';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  styleUrl: './users.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class UsersComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild(DataTableDirective, { static: false }) dtElement!: DataTableDirective;
 
-  dtOptions: ADTSettings = {};
-  dtTrigger: Subject<ADTSettings> = new Subject<ADTSettings>();
+  dtOptions: ADTSettings = {
+    searching: false,
+    destroy: true,
+    language: {
+      lengthMenu: '_MENU_'
+    },
+    columnDefs: [
+      {
+        className: 'text-center',
+        targets: "_all"
+      }
+    ],
+    columns: [
+      { title: $localize `Usuario`, data: 'username' },
+      { title: $localize `Correo`, data: 'email' },
+      { title: $localize `# Entradas`, data: 'numEntries' },
+      { title: $localize `# Eventos`, data: 'numEvents' },
+      { title: $localize `Activo`, 
+        render(data, type, row) {
+          return `<i class="fa-solid ${row.isActive ? 'fa-circle-check' : 'fa-circle-xmark'}"></i>`;
+        }
+      },
+      { title: $localize `Acciones`, data: 'id',
+        render(data) {
+          return `<button class="btn btn-secondary edit-btn" data-id="${data}" data-bs-toggle="modal" data-bs-target="#usersModal"><i class="fa-solid fa-pen-to-square"></i></button>`;
+        }
+      }
+    ]
+  };
 
-  users: IUserEventsInfo[] = [];
+  dtTrigger: Subject<ADTSettings> = new Subject<ADTSettings>();
+  
   userAction!: IUserAction;
 
   constructor (
@@ -26,25 +55,11 @@ export class UsersComponent implements OnInit, AfterViewInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-    this.dtOptions = {
-      searching: false,
-      destroy: true,
-      language: {
-        lengthMenu: '_MENU_'
-      },
-      columnDefs: [
-        {
-          className: 'dt-head-center',
-          targets: "_all"
-        }
-      ]
-    }
-
     this.loaderService.setLoading(true, $localize `Cargando usuarios`);
 
     this.usersService.getAllUsers().subscribe({
       next: (users) => {
-        this.users = users;
+        this.dtOptions.data = users;
         this.rerender();
       }
     }).add(() => {
@@ -54,6 +69,12 @@ export class UsersComponent implements OnInit, AfterViewInit, OnDestroy {
 
   ngAfterViewInit(): void {
     this.dtTrigger.next(this.dtOptions);
+
+    // Delegate click event for edit buttons
+    $(document).on('click', '.edit-btn', (event) => {
+      const userId = $(event.currentTarget).data('id');
+      this.editUser(userId);
+    });
   }
 
   ngOnDestroy(): void {
@@ -72,12 +93,12 @@ export class UsersComponent implements OnInit, AfterViewInit, OnDestroy {
     } as IUserEventsInfo;
 
     if (userAction.isNew) {
-      this.users.push(userEventsInfo);
+      this.dtOptions.data = this.dtOptions.data?.concat(userEventsInfo);
       this.rerender();
     } else {
-      this.users = this.users.map(originalUser => originalUser.id === userAction.user._id ? userEventsInfo : originalUser);
+      this.dtOptions.data = this.dtOptions.data?.map(originalUser => originalUser.id === userInfo._id ? userEventsInfo : originalUser);
     }
-    this.users.sort((a, b) => a.username.toLowerCase().localeCompare(b.username.toLowerCase()));
+    this.dtOptions.data?.sort((a, b) => a.username.toLowerCase().localeCompare(b.username.toLowerCase()));
   }
 
   editUser(userId: string): void {
